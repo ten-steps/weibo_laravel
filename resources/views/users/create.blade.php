438cbf59884c4d8f1ad933ed78ec2143d2da10d2
zr <1085530400@qq.com>
2019-10-17 14:36:55 +0800

    '中文提示消息的显示'

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,11 +5,12 @@
         <div class="card ">
             <div class="card-header">
                 <h5>注册</h5>
+                @include('shared._errors')
             </div>
             <div class="card-body">
                 <form method="POST" action="{{ route('users.store') }}">
                     <div class="form-group">
-                        <label for="name">名称：</label>git
+                        <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
                     </div>
                     {{ csrf_field() }}
