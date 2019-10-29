dd31ceb654da657d01834c3d4b6d2ef1431e80c7
zr <1085530400@qq.com>
2019-10-29 15:43:14 +0800

    '发布微博'

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      */
     protected  $table = 'users';
     protected $fillable = [
-        'name', 'email', 'password',
+       'name', 'email', 'password',
     ];
 
     /**
@@ -52,7 +52,7 @@ class User extends Authenticatable
         });
     }
 
-    public function status()
+    public function statuses()
     {
         return $this->hasMany(Status::class);
     }
